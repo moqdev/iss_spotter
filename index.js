03e33fa97662422ec7bc653ad9c1483e82afd916
Mona Waseem <monaqwaseem@gmail.com>
2021-06-05 15:24:11 +0000

    Add error handling for fetchMyIP fuction.

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 // index.js
-const { fetchMyIP } = require('./iss');
-
-fetchMyIP((error, ip) => {
+// const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
+	
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+	
+//   console.log('It worked! Returned IP:' , ip);
+// });
+	
+// fetchCoordsByIP("99.245.49.160", (error, coordinates) => {
+//     if (error) {
+//         console.log("It didn't work!" , error);
+//         return;
+//     }
+	
+//     console.log('It worked! Returned coordinates: ',coordinates);
+// });
+	
+// fetchISSFlyOverTimes( {latitude: '43.599', longitude: '-79.6885'}, (error,flyOverTimes) => {
+//     if (error) {
+//         console.log("It didn't work!" , error);
+//         return;
+//     }
+	
+//     console.log('It worked! Returned coordinates: ',flyOverTimes);
+// });
+	
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+	
+	
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("It did not work!" , error);
     return;
   }
-
-  console.log('It worked! Returned IP:' , ip);
+	
+  printPassTimes(passTimes);
 });
--- iss.js
+++ iss.js
@@ -7,22 +7,37 @@
 	 *   - The IP address as a string (null if error). Example: "162.245.144.188"
 	 */
 
+
 const request = require("request");
 
-	 
- const fetchMyIP = function(callback) {
-   request('https://api.ipify.org?format=json', (error, response, body)=>{
-     if(error) return "Cannot Connect"
-     if(response.statusCode === 200){
-     const ip = JSON.parse(body)
-      console.log(ip)
-   }
-  })
- }
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body)=>{
+ 
+    //inside the request callback ...
+    //error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // if we get here, all's well and we got the data
+
+    if (response.statusCode === 200) {
+      const ip = JSON.parse(body);
+      console.log(ip);
+    }
+  });
+};
 
 
- fetchMyIP()
+fetchMyIP();
 
 
 
-  module.exports = { fetchMyIP };
+module.exports = { fetchMyIP };
